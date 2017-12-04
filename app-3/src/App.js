105ba92c7fa490fc8e614ca5a885eb3a75376ee2
Ted Kordas <tedkordas@Teds-MacBook-Air.local>
2017-12-04 14:33:22 -0700

    Round3

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -3,16 +3,38 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(){
+    super();
+
+    this.state = {
+      filterString: "",
+      games: [
+        "PUBG",
+        "CS:GO",
+        "Escape From Tarkov",
+        "Hellblade: Senuas Sacrifice",
+        "Rainbow 6 Siege"
+      ]
+    }
+  }
+
+  handleChange(filter) {
+    this.setState({filterString: filter})
+  }
+
   render() {
+    let gametoDisplay = this.state.games.filter((e, i) => {
+      return e.includes(this.state.filterString);
+    }).map((e, i) => {
+      return <h2 key = {i}>{e} </h2>
+    })
+  
+    
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input onChange ={ (e) => this.handleChange(e.target.value) } type = "text" />
+        {gametoDisplay}
       </div>
     );
   }
